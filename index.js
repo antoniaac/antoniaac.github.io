9e87b9bf33c4cbcad9bb4ad1bfaacc1127da7358
Ciont Antonia <ciontantonia@yahoo.com>
2023-01-05 21:08:12 +0200

    display pages with js

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ for (var i = 0; i < skills.length; i++) {
   skillsHTML += "<li>" + skills[i] + "</li>";
 }
 skillsEL.innerHTML = skillsHTML;
+
+function displayHome() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+function displaySkills() {
+  console.warn("trebuie sa afisam skills page");
+  document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+}
+
+function displayLanguages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+
+function displayProjects() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+}
+displayHome();

b32d74d17b339bfd6675d64e1d91f3051f8a75d3
Ciont Antonia <ciontantonia@yahoo.com>
2023-01-05 19:25:08 +0200

    remove comments

---
+++ examples.js
@@ -0,0 +1,25 @@
+console.info("page loaded");
+console.debug(2 * 3 + 1);
+2 * (3 + 1);
+
+var employed = true;
+console.error(employed);
+
+var age = 24;
+
+var user = "Matei";
+console.info(user);
+user = user + "Nicolae";
+console.info(user);
+
+var skills = ["html", "css", "js"];
+
+var dan = {
+  age: 26,
+  employed: true,
+};
+
+var giulia = {
+  age: 24,
+  employed: true,
+};
--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 var skillsEL = document.getElementById("skills-list");
-// skillsEL.style.color = "red";
-// skillsEL.style.fontSize = "25px";
 
-// document.getElementById("skills-list").style.display = none;
-
-// var oldText = skillsEL.innerHTML;
-// skillsEL.innerHTML = oldText + "<li>JS</li>";
-
-// skillsEL.innerHTML = skillsEL.innerHTML + "<li>JS</li>";
-
-// // skillsEL.innerHTML += '<li class="favorite">HTML</li>';
-// skillsEL.innerHTML += '<li class="favorite">CSS</li>';
-// skillsEL.innerHTML += "<li>JS</li>";
-
-var skills = ["HTML", "CSS", "JS"];
-
-// skillsEL.innerHTML += "<li>" + skills[0] + "</li>";
-// skillsEL.innerHTML += "<li>" + skills[1] + "</li>";
-// skillsEL.innerHTML += "<li>" + skills[2] + "</li>";
+var skills = ["HTML", "CSS", "JS", "Drive"];
 
 var skillsHTML = "";
-var i = 0;
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
 
-for (var i = 0; i < skills.length; ) {
+for (var i = 0; i < skills.length; i++) {
   skillsHTML += "<li>" + skills[i] + "</li>";
-  i++;
 }
 skillsEL.innerHTML = skillsHTML;

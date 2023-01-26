7d0c31fd1baebea1ca9676c26d2c226674c04f06
Ciont Antonia <ciontantonia@yahoo.com>
2023-01-26 20:20:33 +0200

    add fetch request to skills json

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var skillsEL = document.getElementById("skills-list");
 
-var skills = [
-  { name: "HTML", endorcements: 5 },
-  { name: "CSS", endorcements: 4 },
-  { name: "JS", endorcements: 3 },
-  { name: "Drive", endorcements: 2 },
-];
+var skills = [];
 
-var skillsHTML = skills.map(function (skill) {
-  // return "<li>" + skill.name + " - " + skill.endorcements + "</li>";
-  return `<li>  ${skill.name}   - <span>${skill.endorcements}</span> </li>`;
+var r1 = fetch("skills.json");
+r1.then(function (raspuns) {
+  var r2 = raspuns.json();
+
+  r2.then(function (skills) {
+    displaySkills(skills);
+  });
 });
 
-skillsEL.innerHTML = skillsHTML.join("");
+function displaySkills(skills) {
+  var skillsHTML = skills.map(function (skill) {
+    // return "<li>" + skill.name + " - " + skill.endorcements + "</li>";
+    return `<li>  ${skill.name}   - <span>${skill.endorcements}</span> </li>`;
+  });
+
+  skillsEL.innerHTML = skillsHTML.join("");
+}
 
 function hideAllPages() {
   var pages = document.querySelectorAll(".page");
